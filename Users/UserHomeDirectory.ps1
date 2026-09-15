584f78ce2e626965c1521eb0cf7df040b7d01229
Adrian Wierzejski <Adrian.Wierzejski@de.obi.net>
2026-09-15 16:42:53 +0200

    Change way of selecting site

--- Users/UserHomeDirectory.ps1
+++ Users/UserHomeDirectory.ps1
@@ -1,58 +1,44 @@
-﻿$logDir = "C:\Temp"
-if($(Test-Path -Path $logDir) -eq $false){
-    $logDir = Get-Location
-}
-$logFilePath = "$($logDir)\HomeDirectory$(Get-Date -Format "MM_dd_yyyy__HH_mm").log"
+﻿$markets=@("0018")
 
-$market=$null
-do{
-    try{
-        [System.Int32]$market=Read-Host "Podaj numer site-u. [Liczba z przedzialu 0-9999]"
-    }catch{
-        Write-Host "Podaj poprawną wartosc"
-        $market=-1
-    }
+$logFilePath = "C:\Temp\Scripts\HomeDirectory$(Get-Date -Format "MM_dd_yyyy__HH_mm").log"
 
-}while($market -lt 0 -or $market -gt 9999)
+foreach($market in $markets){
+    $serverNumber=$market
+    $userFilter="SamAccountName -like `"UUPL$($serverNumber)*`" -or SamAccountName -like `"UYPL$($serverNumber)*`""
+    $serverHostName="XYPL$($serverNumber)001.contoso.local"
+    $serverHomeDir="\\filePL$($serverNumber).global.contoso.local\home$\"
 
-$serverNumber=$($market.toString('0000'))
-$userFilter="SamAccountName -like `"UUPL$($serverNumber)*`" -or SamAccountName -like `"UYPL$($serverNumber)*`""
-$serverHostName="XYpl$($serverNumber)001.contoso.local"
-$serverHomeDir="\\filePL$($serverNumber).global.contoso.local\home$\"
+    Write-Host "Wybrano market to PL$serverNumber"
+    $outFile = "C:\Temp\Scripts\Logs\Site$($serverNumber)_$(Get-Date -Format "MM_dd_yyyy__HH_mm").csv"
 
-Write-Host "Wybrany site to PL$serverNumber"
-
-$users=Get-ADUser -Filter $userFilter -Properties * -Server $serverHostName
-$groupsAD=@()     
-foreach($user in $users){
-    foreach($group in $user.MemberOf){
-        if($group -notin $groupsAD){
-            $groupsAD+=$group
+    $users=Get-ADUser -Filter $userFilter -Properties * -Server $serverHostName
+    $groupsAD=@()     
+    foreach($user in $users){
+        foreach($group in $user.MemberOf){
+            if($group -notin $groupsAD){
+                $groupsAD+=$group
+            }
         }
     }
-}
-$groupsAD = $groupsAD |Sort-Object
-$tableHeader = "SamAccountName,Enabled,HomeDirectory,Title"
-foreach($group in $groupsAD){
-    $tableHeader += $($group -split "," -replace "CN=","," | Select -First 1)
-}
-$tableHeader | out-file -FilePath "$($logDir)\Site$serverNumber.csv" -Append -Encoding default
-foreach($user in $users){
-    $tableRow=""
-    $tableRow +=$($user.SamAccountName)
-    $tableRow +=",$($user.Enabled)"
-    $tableRow +=",$($user.HomeDirectory)"
-    $tableRow +=",$($($user.Title) -replace ",",".")"
+    $groupsAD = $groupsAD |Sort-Object
+    $tableHeader = "SamAccountName,Enabled,HomeDirectory,Title"
     foreach($group in $groupsAD){
-        if($group -in $user.MemberOf){
-            $tableRow+=",True"
-        }else{
-            $tableRow+=",False"
-        }
+        $tableHeader += $($group -split "," -replace "CN=","," | Select -First 1)
     }
-    $tableRow | out-file -FilePath "$($logDir)\Site$serverNumber.csv" -Append -Encoding default
+    $tableHeader | out-file -FilePath $outFile -Append -Encoding default
+    foreach($user in $users){
+        $tableRow=""
+        $tableRow +=$($user.SamAccountName)
+        $tableRow +=",$($user.Enabled)"
+        $tableRow +=",$($user.HomeDirectory)"
+        $tableRow +=",$($($user.Title) -replace ",",".")"
+        foreach($group in $groupsAD){
+            if($group -in $user.MemberOf){
+                $tableRow+=",True"
+            }else{
+                $tableRow+=",False"
+            }
+        }
+        $tableRow | out-file -FilePath $outFile -Append -Encoding default
+    }
 }
-
-
-
-
